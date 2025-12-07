d1bf68bc783d3c2576d21e45c72005e4733558ea
Shipon-Roy <shiponroy509@gmail.com>
2025-12-07 15:25:32 +0600

    some update

--- src/modules/auth/auth.controllers.ts
+++ src/modules/auth/auth.controllers.ts
@@ -24,11 +24,18 @@ const userSignin = async (req: Request, res: Response) => {
 
     const result = await authServices.userSignin(email, password);
 
-    res.status(200).json({
-      success: true,
-      message: "Login successful",
-      data: result,
-    });
+    if (!result) {
+      res.status(401).json({
+        success: false,
+        message: "You aren't user signup first",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "Login successful",
+        data: result,
+      });
+    }
   } catch (error: any) {
     res.status(500).json({
       success: false,
